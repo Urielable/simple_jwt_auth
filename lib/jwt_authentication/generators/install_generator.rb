0b726779de6119c201a8dd2294a25a52553700d0
urielable <uriel85@gmail.com>
2024-06-06 02:29:12 -0600

    Version gem

--- lib/jwt_authentication/generators/install_generator.rb
+++ lib/jwt_authentication/generators/install_generator.rb
@@ -1,3 +1,4 @@
+# lib/jwt_authentication/generators/install_generator.rb
 require 'rails/generators'
 require 'rails/generators/migration'
 
@@ -8,12 +9,13 @@ module JwtAuthentication
 
       source_root File.expand_path('templates', __dir__)
 
-      def copy_migrations
+      def create_migration_file
         migration_template "create_users.rb", "db/migrate/create_users.rb"
       end
 
+      # Implement the required interface for Rails::Generators::Migration.
       def self.next_migration_number(dirname)
-        if ActiveRecord::Base.timestamped_migrations
+        if ActiveRecord::Base.connection.migration_context.timestamped_migrations
           Time.now.utc.strftime("%Y%m%d%H%M%S")
         else
           "%.3d" % (current_migration_number(dirname) + 1)
